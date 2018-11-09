b2f76f7035a5b7fd87812e94a17ee4fe19002a38
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-09 21:41:36 +0530

    removed events and added event handler

--- index.js
+++ index.js
@@ -33,71 +33,17 @@ const loadCommands = module.exports.loadCommands = (dir = "./commands/") => {
 };
 loadCommands();
 
-// Message Event
-bot.on("message", async message => {
-
-//Made to not get the bot token while using eval command
-    var tkn = "bot.token,token".split(",");
-    for (i=0;i<tkn.length;i++) {
-        if (message.content.toLowerCase().includes(tkn[i])) {
-   
-          return;
-        }
-    }
-//end of the token
-	if(message.author.bot) return;
-	if(message.channel.type === "dm") return;
-
-	let prefix = `${botconfig.PREFIX}`;
-	let messageArray = message.content.split(" ");
-	let command = messageArray[0];
-	let args = messageArray.slice(1);
-if (!message.content.startsWith(prefix)) return;
-
-    let commandfile = bot.commands.get(command.slice(prefix.length));
-    if(commandfile) commandfile.run(bot,message,args);
-
- 
-});
-
-//End Of Message Event
-function setGame() {
-    const set = () => {
-        bot.user.setActivity(`for ${botconfig.PREFIX}help ${bot.guild.size} Guilds and ${bot.users.size} Users`, {type:botconfig.stats}).catch(err => console.log(err));
-    };
-    set();
-    setInterval(() => set(), 60 * 60000);
-    bot.user.setStatus(`${botconfig.status}`).catch(console.error);
-}
-
-bot.on('ready', () => { 
-    setGame();
-    getBotStats();
-    console.log(chalk.blue(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds, ${bot.users.size} members, and ${commandSize} commands!`)); 
-    console.log(chalk.green(`Sparky Version: ${botconfig.version}`));
-    console.log(chalk.blue(`Sparky Developers:${botconfig.devs}`));
-    console.log(chalk.green(`Sparky Prefix: ${botconfig.PREFIX}`));
-}); 
-
-
-function getBotStats(status) {
-    const stats = {
-        guilds: bot.guilds.size,
-        users: bot.users.filter(f => !f.bot).size,
-        commands: commandSize,
-        status: !status ? 'Online' : status
-    };
-
-    fs.writeFile('./botstats.json', JSON.stringify(stats, null, 2), null, err => {
-        if (err) console.log(err)
-    }); 
-}
-
-
-
+    fs.readdir("./events/", (err, files) => {
+        if (err) return console.error(err);
+        files.forEach(file => {
+          const event = require(`./events/${file}`);
+          let eventName = file.split(".")[0];
+          bot.on(eventName, event.bind(null, bot));
+        });
+      });
+  
 function closeApp() {
 console.log(chalk.red("Closing App"));
-getBotStats('Offline');
 
 setTimeout(() => process.exit(), 2 * 1000);
 }
