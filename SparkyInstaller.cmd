8a2a29368868477e5af07f92dc842632f4e2f104
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-11 23:08:15 +0530

    Update SparkyInstaller.cmd

--- SparkyInstaller.cmd
+++ SparkyInstaller.cmd
@@ -3,7 +3,10 @@ echo [Sparky]: Cloning Project
 git clone https://github.com/Sparky-Discord-Bot/Sparky
 cd Sparky 
 cls
+echo [Sparky]: Installing Packages
 npm install
+cls
+echo [Sparky]: Setup Bot
 pause
 node sparky.js
 pause
