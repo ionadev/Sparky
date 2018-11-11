47bba8f2fce49321c2000eb625acd676b855ec6b
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-11 16:41:38 +0530

    Update botinfo.js

--- commands/Misc/botinfo.js
+++ commands/Misc/botinfo.js
@@ -11,44 +11,7 @@ const ms = require("ms")
 const botconfig = require('../../botconfig.json');
 
 module.exports.run = (bot, message, args) => {// eslint-disable-line no-unused-vars
-    let emoji = {
-        red:  bot.emojis.find(e => e.name == "red"),
-         green:  bot.emojis.find(e => e.name == "green"),
-         yellow:  bot.emojis.find(e => e.name == "yellow"),
-         purple:  bot.emojis.find(e => e.name == "purple"),
-         cpu:  bot.emojis.find(e => e.name == "cpu"),
-         ram:  bot.emojis.find(e => e.name == "ram"),
-         servers:  bot.emojis.find(e => e.name == "servers"),
-         nodejs:  bot.emojis.find(e => e.name == "nodejs"),
-         arch:  bot.emojis.find(e => e.name == "arch"),
-         chat:  bot.emojis.find(e => e.name == "chat"),
-         discordjs: bot.emojis.find(e => e.name == "discordjs"),
-         uptime: bot.emojis.find(e => e.name == "uptime"),
-         users: bot.emojis.find(e => e.name == "users"),
-         cpuusgae: bot.emojis.find(e =>e.name == "cpuusage"),
-         platform: bot.emojis.find(e => e.name == "platform")
-    };
- 
-
-    let botstatus;
-   // let status;
-    if(botconfig.status === "online") {
-    
-        botstatus = emoji.green;
-    }
-    else if(botconfig.status === "idle") {
-        
-        botstatus = emoji.yellow;
-        }
-        else if(botconfig.status === "dnd") {
-           
-            botstatus = emoji.red;
-            }
-            else {
-                botstatus = emoji.green;
-            }
-    
-            
+          
     let cpuLol;
     cpuStat.usagePercent(function(err, percent, seconds) {
         if (err) {
@@ -60,18 +23,18 @@ module.exports.run = (bot, message, args) => {// eslint-disable-line no-unused-v
             .setAuthor(bot.user.username)
             .setTitle("***BOT Stats***")
             .setColor("RANDOM")
-            .addField("Status-", botstatus)
-            .addField(emoji.ram + `• Mem Usage : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, `**Shows Memory Usage of ${bot.user.username}**`, true)
-            .addField(emoji.uptime + `• Uptime: ${duration}`,`**${bot.user.username} Uptime**`, true)
-            .addField(emoji.users + `• Users: ${bot.users.size.toLocaleString()}`,`**${bot.user.username} Users**`, true)
-            .addField(emoji.servers + `• Servers: ${bot.guilds.size.toLocaleString()}`,`**${bot.user.username} Servers**`, true)
-            .addField(emoji.chat + `• Channels : ${bot.channels.size.toLocaleString()}`,`**${bot.user.username} Channels**`, true)
-            .addField(emoji.discordjs + `• Discord.js: ` + `v${version}`,`**${bot.user.username} Discord.js Version**`, true)
-            .addField(emoji.nodejs + `• Node: ${process.version}`,`**${bot.user.username} Node Version**`)
-            .addField(emoji.cpu + `• CPU: ${os.cpus().map(i => `${i.model}`)[0]}`, `**Shows ${bot.user.username} CPU Usage**`, true)
-            .addField(emoji.cpuusgae + `• CPU usage: ${percent.toFixed(2)}%`, `**${bot.user.username} Cpu Usage**`, true)
-            .addField(emoji.arch + `• Arch:  \`${os.arch()}\``,"**Comupter Arch**", true)
-            .addField(emoji.platform + `• Platform: ${os.platform()}`,`**${bot.user.username} Platform**`, true)
+            .addField("Status-", `**${bot.user.presence.status}**`)
+            .addField(`• Mem Usage : ${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)} / ${(os.totalmem() / 1024 / 1024).toFixed(2)} MB`, `**Shows Memory Usage of ${bot.user.username}**`, true)
+            .addField(`• Uptime: ${duration}`,`**${bot.user.username} Uptime**`, true)
+            .addField(`• Users: ${bot.users.size.toLocaleString()}`,`**${bot.user.username} Users**`, true)
+            .addField(`• Servers: ${bot.guilds.size.toLocaleString()}`,`**${bot.user.username} Servers**`, true)
+            .addField(`• Channels : ${bot.channels.size.toLocaleString()}`,`**${bot.user.username} Channels**`, true)
+            .addField(`• Discord.js: ` + `v${version}`,`**${bot.user.username} Discord.js Version**`, true)
+            .addField(`• Node: ${process.version}`,`**${bot.user.username} Node Version**`)
+            .addField(`• CPU: ${os.cpus().map(i => `${i.model}`)[0]}`, `**Shows ${bot.user.username} CPU Usage**`, true)
+            .addField(`• CPU usage: ${percent.toFixed(2)}%`, `**${bot.user.username} Cpu Usage**`, true)
+            .addField(`• Arch:  \`${os.arch()}\``,"**Comupter Arch**", true)
+            .addField(`• Platform: ${os.platform()}`,`**${bot.user.username} Platform**`, true)
         message.channel.send(embedStats)
     });
 };
@@ -82,7 +45,7 @@ module.exports.command = {
     name: 'botinfo',
     permission: "none",
     description: `Show info about the bot`,
-    usage: "botinfo",
+    usage: `${botconfig.PREFIX}botinfo`,
     category: "MISC",
     enabled: true
 };
