32c4f681c7abdc5e148ad2a78f32555accc38146
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-11 16:44:06 +0530

    rewrite

--- commands/Misc/help.js
+++ commands/Misc/help.js
@@ -1,32 +1,47 @@
 const Discord = require('discord.js');
-
+const botconfig = require("../../botconfig.json");
 module.exports.run = async (bot, message) => {
+    let owner = new Discord.RichEmbed()
+    .setTitle("Owner COMMANDS")
+    .setDescription(`${bot.user.username} Dev Comamnds`)  
+    .setColor("BLUE")
+    .setThumbnail("https://i.imgur.com/6JsuVCH.png")
+    bot.commands.filter(cmd => cmd.command.category === "OWNER").map(cmd => owner.addField(cmd.command.name , `**${cmd.command.description}**`))
+    message.channel.send(owner)
 
-    let emojis = { 
-        on: bot.emojis.find(e => e.name == "on"),
-        off: bot.emojis.find(e => e.name == "off"),
-    }
+    let fun = new Discord.RichEmbed()
+    .setTitle("Fun COMMANDS")
+    .setDescription(`${bot.user.username} Fun Commands`)
+    .setColor("BLUE")
+    .setThumbnail("https://i.imgur.com/vjQ7RUU.png")
+    bot.commands.filter(cmd => cmd.command.category === "FUN").map(cmd => fun.addField(cmd.command.name , `**${cmd.command.description}**`))
+    message.channel.send(fun)
 
+    let guild = new Discord.RichEmbed()
+    .setTitle("Guild COMMANDS")
+    .setDescription(`${bot.user.username} Guild Commands`)  
+    .setColor("BLUE")
+    .setThumbnail("https://i.imgur.com/SuDr9pf.png")
+    bot.commands.filter(cmd => cmd.command.category === "GUILD").map(cmd => guild.addField(cmd.command.name , `**${cmd.command.description}**`))
+    message.channel.send(guild)
 
-let helpembed = new Discord.RichEmbed()
-.setColor("PURPLE")
-.setTitle(`${bot.user.username}`)
-.setDescription(`${bot.user.username} help commands`)
-.addField(emojis.on + "*serverinfo*", `**Gives ${message.guild.name} info**`)
-.addField(emojis.on + "*help*", `**Gives ${bot.user.username} commands**`)
-.addField(emojis.on + "*ping*", `**Gives ${bot.user.username} response time**`)
-.addField(emojis.on + "*botinfo*", `**Gives ${bot.user.username} info or stats**`) 
-    message.channel.send(helpembed)
+    let misc = new Discord.RichEmbed()
+    .setTitle("Misc COMMANDS")
+    .setDescription(`${bot.user.username} Misc Commands`) 
+    .setColor("BLUE")
+    .setThumbnail("https://i.imgur.com/8ObZIQl.png")
+    bot.commands.filter(cmd => cmd.command.category === "MISC").map(cmd => misc.addField(cmd.command.name , `**${cmd.command.description}**`))
+    message.channel.send(misc)
+  
 
 
 
 };
 module.exports.command = {
     name: 'help',
-    permission: "none",
-   // aliases: ["h"],
+    permission: "none";
     description: `Gives Help Commands Of bot`,
-    usage: "help",
+    usage: `{botconfig.PREFIX}help`,
     category: "MISC",
     enabled: true
 }; 
