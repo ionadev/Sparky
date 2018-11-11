63fb9bc612878d1c20dd4a2340ebccafa6a48a1b
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-11 16:48:18 +0530

    Update serverinfo.js

--- commands/Misc/serverinfo.js
+++ commands/Misc/serverinfo.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const botconfig = require("../../botconfig.json")
 module.exports.run = async (bot, message, args) => {
 
 let online = message.guild.members.filter(member => member.user.presence.status === 'online');
@@ -6,37 +7,22 @@ let idle = message.guild.members.filter(member => member.user.presence.status ==
 let dnd = message.guild.members.filter(member => member.user.presence.status === 'dnd');
 let offline = message.guild.members.filter(member => member.user.presence.status === 'offline');
 
-let emoji = {
-    red:  bot.emojis.find(e => e.name == "red"),
-     green:  bot.emojis.find(e => e.name == "green"),
-     yellow:  bot.emojis.find(e => e.name == "yellow"),
-     grey:  bot.emojis.find(e => e.name == "grey"),
-     bot: bot.emojis.find(e => e.name == "bot"),
-     owner: bot.emojis.find(e => e.name == "owner"),
-     chat: bot.emojis.find(e => e.name == "chat"),
-     humans: bot.emojis.find(e => e.name == "humans"),
-     map: bot.emojis.find(e => e.name == "map"),
-     role: bot.emojis.find(e => e.name == "role"),
-	 purple: bot.emojis.find(e => e.name == "purple")
-}
 let sicon = message.guild.iconURL;
    let serverembed = new Discord.RichEmbed()
-   
-
     .setAuthor(message.guild.name, sicon)
     .setFooter(`Server Created at `+ message.guild.createdAt)
     .setColor("GREEN")
     .setThumbnail(sicon)
-    .addField(emoji.owner+" Owner", message.guild.owner.user.tag, true)
-    .addField(emoji.map+" Region",message.guild.region, true)
-    .addField(emoji.chat+" Channels :"+ message.guild.channels.size, "**ServerChannels**", true)
-   .addField(emoji.humans+`Humans : ${message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size}`, "**Server Members**` ", true)
-   .addField(emoji.bot+" Bots :"+ message.guild.members.filter(m => m.user.bot).size, "**Server Bots**", true)
-   .addField(emoji.green+" Online :"+ online.size, "**Online Members**", true)
-   .addField(emoji.yellow+" Idle :"+ idle.size, "**Idle Members**", true)
-   .addField(emoji.red+" Dnd :"+ dnd.size, "**Dnd Members**", true)
-   .addField(emoji.grey+" Offline :"+ offline.size, "**Offline Members**", true)
-   .addField(emoji.role+" Roles :"+ message.guild.roles.size, "**Roles In The Server**", true);
+    .addField(" Owner", message.guild.owner.user.tag, true)
+    .addField(" Region",message.guild.region, true)
+    .addField(" Channels :"+ message.guild.channels.size, "**ServerChannels**", true)
+   .addField(`Humans : ${message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size}`, "**Server Members**` ", true)
+   .addField(" Bots :"+ message.guild.members.filter(m => m.user.bot).size, "**Server Bots**", true)
+   .addField(" Online :"+ online.size, "**Online Members**", true)
+   .addField(" Idle :"+ idle.size, "**Idle Members**", true)
+   .addField(" Dnd :"+ dnd.size, "**Dnd Members**", true)
+   .addField(" Offline :"+ offline.size, "**Offline Members**", true)
+   .addField(" Roles :"+ message.guild.roles.size, "**Roles In The Server**", true);
    message.channel.send(serverembed);
 }
 
@@ -44,6 +30,6 @@ module.exports.command = {
     name: 'serverinfo',
     permission: "none",
     description: "Show info about the serverinfo",
-    usage: "serverinfo",
+    usage: `${botconfig.PREFIX}serverinfo`,
     enabled: true
 };
