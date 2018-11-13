c8d8d3e42371c2ae632d8edce3a8f6ea0c8e3879
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-13 23:16:45 +0530

    improved
    custom prefix setup is done

--- events/message.js
+++ events/message.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 const botconfig = require("../botconfig.json");
+const fs = require("fs");
 module.exports = async(bot, message) => {
     var tkn = "bot.token,token".split(",");
     for (i=0;i<tkn.length;i++) {
@@ -9,15 +10,35 @@ module.exports = async(bot, message) => {
         }
     }
 	if(message.author.bot) return;
-	if(message.channel.type === "dm") return;
+    if(message.channel.type === "dm") return;
+    
+    let prefixes = JSON.parse(fs.readFileSync("./data/prefixes.json", "utf8"));
 
-	let prefix = `${botconfig.PREFIX}`;
-	let messageArray = message.content.split(" ");
-	let command = messageArray[0];
-	let args = messageArray.slice(1);
-if (!message.content.startsWith(prefix)) return;
+    if (!prefixes[message.guild.id]) {
+        prefixes[message.guild.id] = {
+          prefixes: botconfig.PREFIX
+        };
+      }
 
-    let commandfile = bot.commands.get(command.slice(prefix.length));
-    commandfile ? commandfile.run(bot,message,args) : null;
+    let prefix = prefixes[message.guild.id].prefixes;
+    
+    let args = message.content.slice(prefix.length).trim().split(' ');
+    let cmd = args.shift().toLowerCase();
+    let command;
+    
+    if (!message.content.startsWith(prefix)) return;
 
+if (bot.commands.has(cmd)) {
+    command = bot.commands.get(cmd);
+ 
+} else if (bot.aliases.has(cmd)) {
+    command = bot.commands.get(bot.aliases.get(cmd));
+
+}
+try {
+    command.run(bot, message, args);
+
+} catch (e) {
+    return;
+}
 }
