4aa50431ad1b3e38887663bc67072d4271c7f7a8
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-20 20:38:32 +0530

    Update serverinfo.js

--- commands/Misc/serverinfo.js
+++ commands/Misc/serverinfo.js
@@ -4,18 +4,6 @@ let online = message.guild.members.filter(member => member.user.presence.status 
 let idle = message.guild.members.filter(member => member.user.presence.status === 'idle');
 let dnd = message.guild.members.filter(member => member.user.presence.status === 'dnd');
 let offline = message.guild.members.filter(member => member.user.presence.status === 'offline');
-let emoji = {
-    red:  bot.emojis.find(e => e.name === "red"),
-     green:  bot.emojis.find(e => e.name === "green"),
-     yellow:  bot.emojis.find(e => e.name === "yellow"),
-     grey:  bot.emojis.find(e => e.name === "grey"),
-     bot: bot.emojis.find(e => e.name === "bot"),
-     owner: bot.emojis.find(e => e.name === "owner"),
-     chat: bot.emojis.find(e => e.name === "chat"),
-     humans: bot.emojis.find(e => e.name === "humans"),
-     map: bot.emojis.find(e => e.name === "map"),
-     role: bot.emojis.find(e => e.name === "role"),
-};
 let sicon = message.guild.iconURL;
    let serverembed = new Discord.RichEmbed()
    
@@ -24,16 +12,16 @@ let sicon = message.guild.iconURL;
     .setFooter(`Server Created at `+ message.guild.createdAt)
     .setColor("GREEN")
     .setThumbnail(sicon)
-    .addField(emoji.owner+" Owner", message.guild.owner.user.tag, true)
-    .addField(emoji.map+" Region",message.guild.region, true)
-    .addField(emoji.chat+" Channels :"+ message.guild.channels.size, "**ServerChannels**", true)
-   .addField(emoji.humans+`Humans : ${message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size}`, "**Server Members**` ", true)
-   .addField(emoji.bot+" Bots :"+ message.guild.members.filter(m => m.user.bot).size, "**Server Bots**", true)
-   .addField(emoji.green+" Online :"+ online.size, "**Online Members**", true)
-   .addField(emoji.yellow+" Idle :"+ idle.size, "**Idle Members**", true)
-   .addField(emoji.red+" Dnd :"+ dnd.size, "**Dnd Members**", true)
-   .addField(emoji.grey+" Offline :"+ offline.size, "**Offline Members**", true)
-   .addField(emoji.role+" Roles :"+ message.guild.roles.size, "**Roles In The Server**", true);
+    .addField(" Owner", message.guild.owner.user.tag, true)
+    .addField(" Region",message.guild.region, true)
+    .addField(" Channels :"+ message.guild.channels.size, "**ServerChannels**", true)
+   .addField(`Humans : ${message.guild.memberCount - message.guild.members.filter(m => m.user.bot).size}`, "**Server Members**` ", true)
+   .addField(" Bots :"+ message.guild.members.filter(m => m.user.bot).size, "**Server Bots**", true)
+   .addField(" Online :"+ online.size, "**Online Members**", true)
+   .addField(" Idle :"+ idle.size, "**Idle Members**", true)
+   .addField(" Dnd :"+ dnd.size, "**Dnd Members**", true)
+   .addField(" Offline :"+ offline.size, "**Offline Members**", true)
+   .addField(" Roles :"+ message.guild.roles.size, "**Roles In The Server**", true);
    message.channel.send(serverembed);
 };
 
