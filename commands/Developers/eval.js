2fffd8554665c56c21514549fb1768bc6b75679b
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-11-20 20:02:10 +0530

    Add files via upload

--- commands/Developers/eval.js
+++ commands/Developers/eval.js
@@ -1,10 +1,12 @@
-const Discord = require("discord.js");
-const botconfig = require('../../botconfig.json');
-var tkn = "bot.token,token".split(",");
+const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
 
-  if (message.author.id !== botconfig.owner ) return;
+  let emoji = {
+    red:  bot.emojis.find(e => e.name === "red"),
+     green:  bot.emojis.find(e => e.name === "green")
+};
 
+  if(!["259008949427109891","310830923744673803","285149851572895744"].includes(message.author.id)) return;
     function clean(text) {
       if (typeof(text) === "string")
         return text.replace(/'/g, "`" + String.fromCharCode(8203)).replace(/@/g, "@" + String.fromCharCode(8203));
@@ -12,8 +14,8 @@ module.exports.run = async (bot, message, args) => {
           return text;
     }
   
- let argresult = args.join(' ');
- if (message.author.id !== botconfig.owner ) {
+ var argresult = args.join(' ');
+ if(!["259008949427109891","310830923744673803","285149851572895744"].includes(message.author.id)) {
            // Check if user have Permissions to use the command
           message.channel.send('You Don\'t Have Permissions To Use This Command !'); // Send Message to the channel if they dont have permissions
           return; // Returns the code so the rest doesn't run
@@ -25,24 +27,23 @@ module.exports.run = async (bot, message, args) => {
         try {
   
           var evaled = eval(argresult);
-          let token = botconfig.token;
 
           if (typeof evaled !== "string")
          evaled = require("util").inspect(evaled);
  
 
           let embed = new Discord.RichEmbed()
-          .addField(`${bot.user.username} JavaScript Eval Success:`, `** **`)
+          .addField(`${bot.user.username} JavaScript Eval Success:`+emoji.green, `** **`)
           .addField(":inbox_tray: **INPUT**", "```js\n" + args.join(" ") + "```")
           .addField(":outbox_tray: **OUTPUT**", "```" + clean(evaled) + "```")
-          .setColor("#FF5733")
-          .setFooter(message.createdAt, message.author.avatarURL)
-          message.channel.send(embed);
+          .setColor(0xFF5733)
+          .setFooter(message.createdAt, message.author.avatarURL);
+          message.channel.send({embed});
 
         } catch (err){
   
           message.channel.send(new Discord.RichEmbed()
-          .addField(`${bot.user.username} - JavaScript Eval Error:`, "There Was a Problem With The Code That You Are Trying To Run!")
+          .addField(`${bot.user.username} - JavaScript Eval Error:`+emoji.red, "There Was a Problem With The Code That You Are Trying To Run!")
           .addField("Error", "```" + clean(err) + "```")
           .setColor(0xFF5733)
           .setFooter(message.createdAt, message.author.avatarURL))
@@ -50,13 +51,13 @@ module.exports.run = async (bot, message, args) => {
               .catch( error => message.channel.send(`**ERROR:** ${error.message}`))
         }
 
-}
+};
 //Exporting command
 module.exports.command = {
     name: 'eval',
     permission: "ADMINISTRATOR",
     description: "To Compile Javascript Code ",
-    usage: `${botconfig.PREFIX}eval //code`,
+    usage: `eval //code`,
     category: "OWNER",
     enabled: true
 };
