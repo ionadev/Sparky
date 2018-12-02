ffe0dcbabdf065a7ca174f201bdb7b2d13aa153a
Anish Shobith P S <38991749+Anish-Shobith@users.noreply.github.com>
2018-12-02 22:34:59 +0530

    now even supported for dbl

--- events/ready.js
+++ events/ready.js
@@ -1,17 +1,17 @@
-const Discord = require('discord.js');
-const fs = require('fs');
-const chalk = require('chalk');
+const Discord = require("discord.js");
+const fs = require("fs");
+const chalk = require("chalk");
 const botconfig = require("../botconfig.json");
+const Dbl = require("dblapi.js");
 module.exports = async (bot) => {
-    
+  const dbl = new Dbl(botconfig.dbl, bot);
     
         setGame();
-        getBotStats();
         console.log(chalk.blue(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds, ${bot.users.size} members, and ${bot.commands.size} commands!`)); 
         console.log(chalk.green(`Sparky Version: ${botconfig.version}`));
         console.log(chalk.blue(`Sparky Developers:${botconfig.devs}`));
         console.log(chalk.green(`Sparky Prefix: ${botconfig.PREFIX}`));
-
+dbl.postStats(bot.guilds.size);
     function setGame() {
         const set = () => {
             bot.user.setActivity(`for ${botconfig.PREFIX}help ${bot.guilds.size} Guilds and ${bot.users.size} Users`, {type:botconfig.stats}).catch(err => console.log(err));
@@ -19,19 +19,7 @@ module.exports = async (bot) => {
         set();
         setInterval(() => set(), 60 * 60000);
         bot.user.setStatus(`${botconfig.status}`).catch(console.error);
+
     }
-    
-    function getBotStats(status) {
-        const stats = {
-            guilds: bot.guilds.size,
-            users: bot.users.filter(f => !f.bot).size,
-            commands: bot.commands.size,
-            status: status ? status : 'Online'
-        };
-    
-        fs.writeFile('../botstats.json', JSON.stringify(stats, null, 2), null, err => {
-            if (err) console.log(err)
-        }); 
-    }
-    
-}
+
+};
